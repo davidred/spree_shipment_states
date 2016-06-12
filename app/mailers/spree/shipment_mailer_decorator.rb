51f2bf88a72b3f27675492ad980ce80596e86d9b
David Rozenberg <daveroze@gmail.com>
2016-06-11 21:57:41 -0400

    update pickup and dropoff emails

--- app/mailers/spree/shipment_mailer_decorator.rb
+++ app/mailers/spree/shipment_mailer_decorator.rb
@@ -2,14 +2,14 @@ Spree::ShipmentMailer.class_eval do
   def picked_up_email(shipment, resend=false)
     @shipment = shipment.respond_to?(:id) ? shipment : Spree::Shipment.find(shipment)
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-    subject += "#{Spree::Store.current.name} #{Spree.t('shipment_mailer.pickup_email.subject')} ##{@shipment.order.number}"
+    subject += "#{Spree::Store.current.name} #{Spree.t('shipment_mailer.picked_up_email.subject')} ##{@shipment.order.number}"
     mail(to: @shipment.order.email, from: from_address, subject: subject)
   end
 
   def dropped_off_email(shipment, resend=false)
     @shipment = shipment.respond_to?(:id) ? shipment : Spree::Shipment.find(shipment)
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-    subject += "#{Spree::Store.current.name} #{Spree.t('shipment_mailer.dropoff_email.subject')} ##{@shipment.order.number}"
+    subject += "#{Spree::Store.current.name} #{Spree.t('shipment_mailer.dropped_off_email.subject')} ##{@shipment.order.number}"
     mail(to: @shipment.order.email, from: from_address, subject: subject)
   end
 end
